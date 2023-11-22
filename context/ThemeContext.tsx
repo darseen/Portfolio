e58ef2d8bd0a54a830c9579b6da4e7739b4b239e
Mohammed Alsammarrai <105995943+Darseen@users.noreply.github.com>
2023-11-23 01:02:55 +0300

    Fix theme

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -41,10 +41,7 @@ export default function ThemeContextProvider({
       if (localTheme === "dark") {
         document.documentElement.classList.add("dark");
       }
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-      document.documentElement.classList.add("dark");
-    }
+    } 
   }, []);
 
   return (
